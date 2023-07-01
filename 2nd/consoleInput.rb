1f50d98b606253df7ec023c287558f4c3589d58c
Vadim Voichenko <vvojchenko0@gmail.com>
2023-07-01 21:51:45 +0300

    Added realization for 2nd task

---
+++ 2nd/consoleInput.rb
@@ -0,0 +1,71 @@
+FIRST_TASK_PATH = "./1st/validation"
+# 'C:\Users\vvojc\OneDrive\Desktop\IFTKN\RUBY\SummerPractice_Ruby_SoftServe\1st\validation'
+
+require FIRST_TASK_PATH
+
+module ConsoleInput
+  def get_string_input()
+    input = nil
+    until input && input.match?(/\A[a-zа-щьюяґєіїA-ZА-ЩЬЮЯҐЄІЇ]+\z/)
+      print "Input alphabetic value: "
+      input = gets.chomp
+    end
+    input
+  end
+
+  def get_name_input()
+    input = nil
+    until input && valid_name?(input)
+      print "Input name-typed value (ex. Anna-Maria): "
+      input = gets.chomp
+    end
+    input
+  end
+
+  def get_integer_input()
+    input = nil
+    until input && input.match?(/\A[0-9]+\z/)
+      print "Input integer value: "
+      input = gets.chomp
+    end
+    input.to_i
+  end
+
+  def get_inn_input()
+    input = nil
+    until input && valid_inn?(input)
+      print "Input inn-typed value (ex. AZ0123456789): "
+      input = gets.chomp
+    end
+    input
+  end
+
+  def valid_date_format?(input)
+    /^\d{4}-\d{2}-\d{2}$/.match?(input) && Date.parse(input)
+  rescue ArgumentError
+    false
+  end
+
+  def get_date_input()
+    input = nil
+    until input && valid_date_format?(input)
+      print "Input valid date value in yyyy-mm-dd format: "
+      input = gets.chomp
+    end
+    Date.parse(input)
+  end
+
+  private :valid_date_format?
+end
+
+# string_result = Object.extend(ConsoleInput).get_string_input()
+# p string_result
+# p string_result.class
+
+# integer_result = Object.extend(ConsoleInput).get_integer_input
+# p integer_result
+# p integer_result.class
+
+# date_result = Object.extend(ConsoleInput).get_date_input
+# p date_result
+# p date_result.class
---
+++ 2nd/personRepository.rb
@@ -0,0 +1,82 @@
+require_relative "person"
+
+class PersonRepository
+  def initialize
+    @people = []
+  end
+
+  def add_person(person)
+    if person.is_a?(Person)
+      @people << person
+    else
+      raise ArgumentError, "Person must be a member of Person.Class"
+    end
+  end
+
+  def edit_person_by_inn(inn, new_first_name, new_last_name, new_birth_date)
+    person = get_byINN(inn)
+    if person
+      person.first_name = new_first_name
+      person.last_name = new_last_name
+      person.birth_date = new_birth_date
+    else
+      puts "Person not found for INN: #{inn}"
+    end
+  end
+
+  def delete_person_by_inn(inn)
+    person = get_byINN(inn)
+    if person
+      @people.delete(person)
+      puts "Person with INN #{inn} deleted."
+    else
+      puts "Person not found for INN: #{inn}"
+    end
+  end
+
+  def show_people
+    @people.each do |person|
+      puts "First Name: #{person.first_name}, Last Name: #{person.last_name}, INN: #{person.inn}, Date Of Birth: #{person.birth_date}"
+    end
+  end
+
+  def get_all
+    @people
+  end
+
+  def get_byINN(inn)
+    @people.find { |person| person.inn == inn }
+  end
+
+  def get_by_part_name(name_part)
+    @people.select { |person| person.first_name.include?(name_part) || person.last_name.include?(name_part) }
+  end
+
+  def get_by_date_range(date_from, date_to)
+    if date_from.nil? && date_to.nil?
+      @people
+    elsif date_from.nil?
+      @people.select { |person| person.birth_date <= date_to }
+    elsif date_to.nil?
+      @people.select { |person| person.birth_date >= date_from }
+    else
+      @people.select { |person| person.birth_date >= date_from && person.birth_date <= date_to }
+    end
+  end
+
+end
+
+pr = PersonRepository.new
+pr.add_person(Person.new("John", "Luke", "AZ0123456789", "1999-12-12"))
+pr.add_person(Person.new("Michael", "Buh", "AU9875642310", "1920-12-12"))
+pr.add_person(Person.new("Liza", "Dovgan", "OI0654789321", "2003-12-12"))
+pr.add_person(Person.new("Kulka", "Mica", "AS4563210789", "1999-12-12"))
+pr.add_person(Person.new("Oleg", "Olegius", "AZ0123456789", "2005-12-12"))
+
+pr.show_people
+puts "\n"
+p pr.get_byINN("OI0654789321")
+puts "\n"
+p pr.get_by_part_name("u")
+puts "\n"
+p pr.get_by_date_range(Date.parse("1970-01-01"), Date.parse("2000-01-01"))
