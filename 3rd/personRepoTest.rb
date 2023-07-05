e465fbd1d3695cd49bb1bf6edaa5215f347ec63d
Vadim Voichenko <vvojchenko0@gmail.com>
2023-07-05 17:50:44 +0300

    Added test for main classes

---
+++ 3rd/personRepoTest.rb
@@ -0,0 +1,122 @@
+require "test/unit"
+require "./2nd/person"
+require "./2nd/personRepository"
+
+class PersonRepositoryTest < Test::Unit::TestCase
+  def setup
+    @repository = PersonRepository.new
+    @person1 = Person.new("John", "Doe", "AZ0123456789", "1990-01-01")
+    @person2 = Person.new("Jane", "Smith", "BY9876543210", "1985-05-05")
+    @person_unexist = Person.new("Unexist", "Person", "AB0123456789", "2003-01-18")
+    @repository.add_person(@person1)
+    @repository.add_person(@person2)
+  end
+
+  def test_add_person
+    person3 = Person.new("Alex", "Johnson", "AQ9876543210", "1995-07-10")
+    @repository.add_person(person3)
+    assert_equal(3, @repository.people.length)
+    assert_includes(@repository.people, person3)
+  end
+
+  def test_add_existing_person
+    duplicate_person = Person.new("John", "Doe", "AZ0123456789", "1990-01-01")
+    assert_raise(PersonAlreadyExist) { @repository.add_person(duplicate_person) }
+  end
+
+  def test_add_invalid_person
+    invalid_person = "Invalid Person"
+    assert_raise(ArgumentError) { @repository.add_person(invalid_person) }
+  end
+
+  def test_edit_person_by_inn
+    new_first_name = "Updated"
+    new_last_name = "Name"
+    new_birth_date = "2000-12-31"
+    edited_inn = "AZ0123456789"
+    new_person = Person.new(new_first_name,new_last_name,edited_inn,new_birth_date)
+    @repository.edit_person_by_inn("AZ0123456789", new_person)
+    updated_person = @repository.get_byINN("AZ0123456789")
+    assert_equal(new_first_name, updated_person.first_name)
+    assert_equal(new_last_name, updated_person.last_name)
+    assert_equal(Date.parse(new_birth_date), updated_person.birth_date)
+  end
+
+  def test_edit_nonexistent_person
+    assert_raise(PersonNotFoundError) do
+      @repository.edit_person_by_inn("nonexistent_inn", @person_unexist)
+    end
+  end
+
+  def test_delete_person_by_inn
+    @repository.delete_person_by_inn("AZ0123456789")
+    assert_equal(1, @repository.people.length)
+    assert_not_includes(@repository.people, @person1)
+  end
+
+  def test_delete_nonexistent_person
+    assert_raise(PersonNotFoundError) { @repository.delete_person_by_inn("nonexistent_inn") }
+  end
+
+  def test_get_all
+    all_people = @repository.get_all
+    assert_equal(2, all_people.length)
+    assert_includes(all_people, @person1)
+    assert_includes(all_people, @person2)
+  end
+
+  def test_get_by_inn
+    person = @repository.get_byINN("AZ0123456789")
+    assert_equal(@person1, person)
+  end
+
+  def test_get_by_inn_nonexistent
+    person = @repository.get_byINN("nonexistent_inn")
+    assert_nil(person)
+  end
+
+  def test_get_by_part_name
+    matching_people = @repository.get_by_part_name("jo")
+    assert_equal(1, matching_people.length)
+    assert_includes(matching_people, @person1)
+    assert_not_includes(matching_people, @person2)
+
+    non_matching_people = @repository.get_by_part_name("xyz")
+    assert_empty(non_matching_people)
+  end
+
+  def test_get_by_date_range
+    date_from = "1980-01-01"
+    date_to = "1995-12-31"
+    people_in_range = @repository.get_by_date_range(date_from, date_to)
+    assert_equal(2, people_in_range.length)
+    assert_includes(people_in_range, @person1)
+    assert_includes(people_in_range, @person2)
+
+    date_from = "1990-01-01"
+    date_to = "2000-12-31"
+    people_in_range = @repository.get_by_date_range(date_from, date_to)
+    assert_equal(1, people_in_range.length)
+    assert_includes(people_in_range, @person1)
+    assert_not_includes(people_in_range, @person2)
+
+    date_from = nil
+    date_to = "1989-12-31"
+    people_in_range = @repository.get_by_date_range(date_from, date_to)
+    assert_equal(1, people_in_range.length)
+    assert_includes(people_in_range, @person2)
+
+    date_from = "1990-01-01"
+    date_to = nil
+    people_in_range = @repository.get_by_date_range(date_from, date_to)
+    assert_equal(1, people_in_range.length)
+    assert_includes(people_in_range, @person1)
+
+    date_from = nil
+    date_to = nil
+    all_people = @repository.get_by_date_range(date_from, date_to)
+    assert_equal(2, all_people.length)
+    assert_includes(all_people, @person1)
+    assert_includes(all_people, @person2)
+  end
+end
--- 3rd/personTest.rb
+++ 3rd/personTest.rb
@@ -1,23 +1,75 @@
 require "test/unit"
-require "./TestUnitsPractice/personTest"
-require "date"
 require "./2nd/person"
+
 class PersonTest < Test::Unit::TestCase
 
-  def setup 
+  def setup
     @person = Person.new("Baron", "Toy", "AZ0123456789", Date.today.prev_day)
   end
 
-  def test_valid_first_name
-    expected = "John-Bick"
-    @person.first_name = expected
-    actual = @person.first_name
-    assert_equal(expected, actual, "Name #{expected} is valid name")
+  ['Anna-A', 'An', 'Anna', 'Anna-Maria', 'Каріна-Ірина'].each do |name|
+    define_method("test_valid_first_name_#{name}") do
+      expected = name
+      @person.first_name = expected
+      actual = @person.first_name
+      assert_equal(expected, actual, "Name #{expected} is valid name")
+    end
   end
 
-  def test_invalid_first_name
-    expected = "John-"
-    assert_raise { @person.first_name = expected }
+  ['An-','-','aN','AnnA','-An','A-a','a-A','a-a', '', '123'].each do |name|
+    define_method("test_invalid_first_name_#{name}") do
+      assert_raise { @person.first_name = name }
+    end
+  end
+
+  ['Anna-A', 'An', 'Anna', 'Anna-Maria', 'Каріна-Ірина'].each do |name|
+    define_method("test_valid_last_name_#{name}") do
+      expected = name
+      @person.last_name = expected
+      actual = @person.last_name
+      assert_equal(expected, actual, "Name #{expected} is valid name")
+    end
+  end
+
+  ['An-','-','aN','AnnA','-An','A-a','a-A','a-a', '', '123'].each do |name|
+    define_method("test_invalid_last_name_#{name}") do
+      assert_raise { @person.last_name = name }
+    end
+  end
+
+  ['AZ0123456789', 'BY9876543210', 'CC6549873210', 'EF9874056321'].each do |inn|
+    define_method("test_valid_inn_#{inn}") do
+      expected = inn
+      @person.inn = expected
+      actual = @person.inn
+      assert_equal(expected,actual, "INN #{inn} is valid INN")
+    end
+  end
+
+  ['AZ1', 'AC12', 'AB123', 'HS123456','AZ123456879', 'HZ', '',
+   'Hz1234567890', 'nM7894561230', '1H7894561230', 'H21234567890'].each do |inn|
+    define_method("test_invalid_inn_#{inn}") do
+      assert_raise {@person.inn = inn}
+    end
+  end
+
+  ['1999-01-01', '2003-01-18', '2018-12-03', '1920-02-03'].each do |date|
+    define_method("test_valid_date_#{date}") do
+      expected = date
+      @person.birth_date = expected
+      expected = Date.parse(expected)
+      actual = @person.birth_date
+      assert_equal(expected, actual, "Date #{date} is valid date")
+    end
+  end
+
+  ['2100-01-01', '2200-01-01', '2300-01-01', '',
+   'a', '123', '01/12/2003', '01-01-2001'].each do |date|
+    define_method("test_invalid_date_#{date}") do
+      assert_raise(ArgumentError, "Invalid birth date: #{date}") do
+        @person.birth_date = date
+      end
+    end
   end
 
 end
